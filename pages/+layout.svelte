7385cc6f272a2fcf3b3f6d5fabec95bfd721d69b
Will Lachance <wlach@protonmail.com>
2025-04-21 22:09:03 -0400

    Links

---
+++ pages/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "@evidence-dev/tailwind/fonts.css";
+  import "../app.css";
+  import { EvidenceDefaultLayout } from "@evidence-dev/core-components";
+  export let data;
+</script>
+
+<EvidenceDefaultLayout
+  {data}
+  githubRepo="https://github.com/wlach/gtha-electrification"
+  builtWithEvidence
+>
+  <slot slot="content" />
+</EvidenceDefaultLayout>
